8979149e18fc11f3281c2ab2316d07b5d2dff76c
Hosu Kim <hoskim@c1r10s5.42prague.com>
2024-12-25 22:43:27 +0100

    commit

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 18:41:03 by hoskim            #+#    #+#             */
-/*   Updated: 2024/12/24 19:12:50 by hoskim           ###   ########.fr       */
+/*   Updated: 2024/12/25 19:43:04 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,34 @@
 
 int	main(int argc, char *argv[])
 {
-	char	*pid;
-	char	*msg;
+	char			*pid;
+	char			*msg;
+	unsigned int	**msg_in_bit;
+	int				i;
+	int				j;
 
-	pid = argv[1];
-	msg = argv[2];
+	pid = &argv[1];
+	msg = &argv[2];
 	if (argc != 3)
 	{
-		printf("Please include only two parameters: 1. PID 2. a meassage");
+		printf("Please include only two parameters: 1. Server's PID \
+				2. A meassage\n");
 		return (1);
 	}
-	printf("%s", pid);
+	i = 0;
+	j = 0;
+	while (*msg)
+	{
+		if (*msg % 2 == 1)
+		{
+			msg_in_bit[i][j] = msg_in_bit[i][j] * 10 + 1;
+			j++;
+		}
+		else if (*msg % 2 == 0)
+		{
+			msg_in_bit[i][j] = msg_in_bit[i][j] * 10 + 0;
+			j++;
+		}
+		i++;
+	}
 }
---
+++ src/note.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+
+
+char	**cvt_storage_allotator(char *msg)
+{
+	int		msg_len;
+	char	**cvt_storage;
+
+	msg_len = 0;
+	msg_len = strlen(msg);
+	cvt_storage = malloc(msg_len * sizeof(char *));
+	while (msg_len > 0)
+	{
+		cvt_storage[msg_len - 1] = malloc(9 * sizeof(char));
+		msg_len--;
+	}
+	return (cvt_storage);
+}
+
+char	**bit_coverter(char *msg)
+{
+	int		str_index;
+	int		bit_index;
+	int		char_in_ascii;
+	char	**cvt_storage;
+
+	cvt_storage = cvt_storage_allotator(msg);
+	str_index = 0;
+	char_in_ascii = 0;
+	while (msg[str_index] != '\0')
+	{
+		char_in_ascii = msg[str_index];
+		bit_index = 7;
+		while (bit_index >= 0)
+		{
+			if (char_in_ascii & (1 << bit_index))
+				cvt_storage[str_index][7 - bit_index] = '1';
+			else
+				cvt_storage[str_index][7 - bit_index] = '0';
+			bit_index--;
+		}
+		cvt_storage[str_index][8] = '\0';
+		str_index++;
+	}
+	return (cvt_storage);
+}
+// for (int k = 0; k < str_index; k++)
+//     	printf("%s\n", cvt_storage[k]);
+
+int	main(void)
+{
+	char	*str = "Hello.";
+	char	**result = bit_coverter(str);
+
+	for (int i = 0; result[i] != NULL; i++) {
+        free(result[i]);
+    }
+    free(result);
+    
+	return (0);
+}
