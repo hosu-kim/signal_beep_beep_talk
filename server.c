1ff917e216cf2c4071a6a4e33b8dcf4a38dcc10f
Hosu Kim <hosu@outlook.cz>
2025-01-14 21:55:22 +0100

    commit

--- server.c
+++ server.c
@@ -6,20 +6,20 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 23:22:06 by hoskim            #+#    #+#             */
-/*   Updated: 2025/01/13 19:01:16 by hoskim           ###   ########.fr       */
+/*   Updated: 2025/01/14 21:10:15 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_putpid(pid_t n)
+static void	pid_printer(pid_t n)
 {
 	char	c;
 
 	if (n > 9)
 	{
-		ft_putpid((n / 10));
-		ft_putpid((n % 10));
+		pid_printer((n / 10));
+		pid_printer((n % 10));
 	}
 	else
 	{
@@ -31,15 +31,15 @@ static void	ft_putpid(pid_t n)
 static void	signal_handler(int signal)
 {
 	static char	character;
-	static int	i;
+	static int	bit_counter;
 
-	i++;
+	bit_counter++;
 	if (signal == SIGUSR1)
 		character = character | 1;
-	if (i == 8)
+	if (bit_counter == 8)
 	{
 		write(1, &character, 1);
-		i = 0;
+		bit_counter = 0;
 		character = 0;
 	}
 	character = character << 1;
@@ -54,7 +54,7 @@ int	main(void)
 	write(1, "Waiting for message...\n", 24);
 	write(1, "-----------------------------------------------------------", 60);
 	write(1, "\nServer's PID: ", 16);
-	ft_putpid(server_pid);
+	pid_printer(server_pid);
 	write(1, "\n", 1);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
