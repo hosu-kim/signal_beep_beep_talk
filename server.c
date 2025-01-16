5b6888f27f3aae3d90f0f1a46836e5d1223bc7c0
Hosu Kim <hosu@outlook.cz>
2025-01-16 18:46:18 +0100

    commit

--- server.c
+++ server.c
@@ -1,58 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/16 18:38:45 by hoskim            #+#    #+#             */
-/*   Updated: 2025/01/16 18:38:45 by hoskim           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minitalk.h"
 
 static void	ft_handler(int signal)
 {
-	static int	bit; // 비트를 얼마나 받았는 지 확인하는 정적 변수
-	static char	tmp; // 문자값을 저장하는 정적 변수
+    static int	bit;
+    static char	tmp;
 
-	if (signal == SIGUSR1) // SIGUSR2 일 때는 어차피 0이므로 pass 가능
-		tmp |= (1 << bit); // 가장 오른쪽 비트부터 추가해줌
-	bit++;
-	if (bit == 8) // 비트가 8이 될 경우 저장된 문자를 출력하고 정적변수 초기화
-	{
-		ft_putchar_fd(tmp, 1);
-		bit = 0;
-		tmp = 0;
-	}
+    if (signal == SIGUSR1)
+        tmp |= (1 << bit);
+    bit++;
+    if (bit == 8)
+    {
+        ft_putchar_fd(tmp, 1);
+        bit = 0;
+        tmp = 0;
+    }
 }
 
-static void	ft_pid_print(pid_t pid) // pid 출력
+static void	ft_pid_print(void)
 {
-	ft_putstr_fd("PID -> ", 1);
-	ft_putnbr_fd(pid, 1);
-	ft_putchar_fd('\n', 1);
-	ft_putstr_fd("Waiting for a message...\n", 1);
+    pid_t pid;
+
+    pid = getpid();
+    if (pid == -1)
+    {
+        ft_putstr_fd("Error: Cannot get PID\n", 2);
+        exit(1);
+    }
+    ft_putstr_fd("PID -> ", 1);
+    ft_putnbr_fd(pid, 1);
+    ft_putchar_fd('\n', 1);
+    ft_putstr_fd("Waiting for a message...\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t	pid;
-
-	(void)argv;
-	if (argc != 1) // 인자가 1개일 경우 예외처리
-	{
-		ft_putstr_fd("Error: worng format.\n", 1);
-		ft_putstr_fd("Try: ./server\n", 1);
-		return (0);
-	}
-	pid = getpid(); // pid를 받아오기
-	ft_pid_print(pid); // pid 출력
-    // SIGUSR1신호와 SIGUSR2 신호 입력시 ft_handler함수 호출
-	signal(SIGUSR1, ft_handler);
-	signal(SIGUSR2, ft_handler);
-	while (argc == 1)
-		pause(); // 신호가 입력될 때 까지 대기
-	return (0);
+    if (argc != 1)
+    {
+        ft_putstr_fd("Error: This program doesn't take any arguments\n", 2);
+        return (1);
+    }
+    (void)argv;
+    ft_pid_print();
+    if (signal(SIGUSR1, ft_handler) == SIG_ERR || 
+        signal(SIGUSR2, ft_handler) == SIG_ERR)
+    {
+        ft_putstr_fd("Error: Cannot set signal handlers\n", 2);
+        return (1);
+    }
+    while (1)
+        pause();
+    return (0);
 }
