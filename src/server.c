b32b9bf70c8fbbbc5b3539b8a933832f15df2b58
hoskim <hoskim@student.42prague.com>
2025-01-17 21:35:31 +0100

    commit

--- src/server.c
+++ src/server.c
@@ -3,22 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/17 21:29:25 by hoskim            #+#    #+#             */
-/*   Updated: 2025/01/17 21:29:25 by hoskim           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
 /*   By: hosu-kim <hosu-kim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/17 20:28:44 by hosu-kim          #+#    #+#             */
-/*   Updated: 2025/01/17 20:28:44 by hosu-kim         ###   ########.fr       */
+/*   Created: 2025/01/17 20:34:49 by hosu-kim          #+#    #+#             */
+/*   Updated: 2025/01/17 20:34:49 by hosu-kim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +30,24 @@ void	ft_putnbr(int n)
 	ft_putchar(n % 10 + '0');
 }
 
+static void	reset_buffer(char *str, int *i, int *bit)
+{
+	ft_putstr(str);
+	ft_putchar('\n');
+	*i = 0;
+	*bit = 0;
+	ft_memset(str, 0, 10000);
+}
+
+static void	send_ack(pid_t pid)
+{
+	if (kill(pid, SIGUSR2) == -1)
+	{
+		ft_putstr("Error: Failed to send acknowledgment signal\n");
+		exit(1);
+	}
+}
+
 static void	handle_signal(int signum, siginfo_t *info, void *context)
 {
 	static int		bit = 0;
@@ -55,24 +61,14 @@ static void	handle_signal(int signum, siginfo_t *info, void *context)
 	if (bit == 8)
 	{
 		if (str[i] == '\0')
-		{
-			ft_putstr(str);
-			ft_putchar('\n');
-			i = 0;
-			bit = 0;
-			ft_memset(str, 0, sizeof(str));
-		}
+			reset_buffer(str, &i, &bit);
 		else
 		{
 			bit = 0;
 			i++;
 		}
 	}
-	if (kill(info->si_pid, SIGUSR2) == -1)
-	{
-		ft_putstr("Error: Failed to send acknowledgment signal\n");
-		exit(1);
-	}
+	send_ack(info->si_pid);
 }
 
 int	main(void)
@@ -85,7 +81,7 @@ int	main(void)
 	sa.sa_sigaction = handle_signal;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1 || \
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
 		sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
 		ft_putstr("Error: Failed to set up signal handlers\n");
