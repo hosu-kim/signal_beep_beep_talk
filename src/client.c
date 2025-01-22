23c777af3ecf12ea42ca9295995986078e6a85ee
Hosu Kim <hosu@outlook.cz>
2025-01-22 19:42:23 +0100

    commit

--- src/client.c
+++ src/client.c
@@ -6,34 +6,34 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 20:43:08 by hoskim            #+#    #+#             */
-/*   Updated: 2025/01/22 11:37:24 by hoskim           ###   ########.fr       */
+/*   Updated: 2025/01/22 19:12:00 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	send_char(pid_t server_pid, char c, int bit_index)
+void	send_char_as_bits(pid_t server_pid, char character, int bit_position)
 {
-	if (bit_index < 8)
+	if (bit_position < 8)
 	{
-		if ((c >> bit_index) & 1)
+		if ((character >> bit_position) & 1)
 			kill(server_pid, SIGUSR1);
 		else
 			kill(server_pid, SIGUSR2);
 		usleep(700);
-		send_char(server_pid, c, bit_index + 1);
+		send_char_as_bits(server_pid, character, bit_position + 1);
 	}
 }
 
-void	send_string(pid_t server_pid, char *str)
+void	iterate_string(pid_t server_pid, char *str)
 {
 	if (*str)
 	{
-		send_char(server_pid, *str, 0);
-		send_string(server_pid, str + 1);
+		send_char_as_bits(server_pid, *str, 0);
+		iterate_string(server_pid, str + 1);
 	}
 	else
-		send_char(server_pid, '\0', 0);
+		send_char_as_bits(server_pid, '\0', 0);
 }
 
 int	main(int argc, char **argv)
@@ -51,6 +51,6 @@ int	main(int argc, char **argv)
 		ft_putstr("Error: Invalid server PID\n");
 		return (1);
 	}
-	send_string(server_pid, argv[2]);
+	iterate_string(server_pid, argv[2]);
 	return (0);
 }
